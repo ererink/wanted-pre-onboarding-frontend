1dc572449d24bbc3e033a4d811d3c78c2c8097bb
ererink <yelkim0210@gmail.com>
2023-08-09 17:00:23 +0900

    feat: 할 일 목록 출력

--- client/src/@components/todo/todoList.jsx
+++ client/src/@components/todo/todoList.jsx
@@ -3,10 +3,9 @@ import styled from 'styled-components';
 import { useNavigate } from "react-router-dom";
 
 function TodoList(){
+    const [todoItems, setTodoItems] = useState([]);
     const userToken = localStorage.getItem("jwt-token");
     const navigate = useNavigate();
-    
-    const [todoItems, setTodoItems] = useState([]);
 
     useEffect(() => {
         // 로그인을 하지 않은 상태로 로그인한 경우 /signin 페이지로 리다이렉트
@@ -14,14 +13,25 @@ function TodoList(){
             window.alert('로그인이 필요한 페이지입니다!')
             navigate('/signin');
         }
-      }, [userToken, navigate]);
-    console.log(userToken);
-    
 
+        // 할 일 목록을 가져오는 API 요청
+        fetch('/todos', {
+            headers: {'Authorization': `Bearer ${userToken}`}
+        })
+        .then(response => response.json())
+        .then(data => {
+            // 서버에서 받아온 데이터로 todoItems 업데이트
+            setTodoItems(data);
+        })
+        .catch(error => {
+            console.log('API 요청 에러:', error);
+        });
+      }, [userToken, navigate]);
+    
     // 완료된 할 일 처리
     const handleTodoComplete = (todoId) => {
         const updatedTodoItem = todoItems.map((todo) => 
-            todo.id === todoId ? { ...todo, completed: !todo.completed } : todo
+            todo.id === todoId ? { ...todo, isCompleted: !todo.isCompleted } : todo
         );
         setTodoItems(updatedTodoItem);
     }
@@ -32,12 +42,12 @@ function TodoList(){
             <TodoItems>
                {todoItems.map((todo) =>(
                     <TodoItem key={todo.id}>
-                        <lable>
+                        <label>
                             <input type='checkbox'
-                                   checked={todo.completed}
+                                   checked={todo.isCompleted}
                                    onChange={() => handleTodoComplete(todo.id)}/>
-                            <span>{todo.text}</span>
-                        </lable>
+                            <span>{todo.todo}</span>
+                        </label>
                     </TodoItem>
                 ))} 
             </TodoItems>

dd3a02868d299b66b8bf2941b870191879f36b09
ererink <yelkim0210@gmail.com>
2023-08-11 15:47:03 +0900

    feat: Todo list 페이지 UI 적용

--- client/src/@components/todo/todoInput.jsx
+++ client/src/@components/todo/todoInput.jsx
@@ -41,20 +41,70 @@ function TodoInput(props) {
     };
 
     return(
-        <>
+        <TodoInputWrapper>
             <form onSubmit={handleAddTodo}>
-                <input data-testid="new-todo-input"
-                       onChange={handleTodoChange}
-                       value={todoItem}
-                       placeholder='오늘의 할 일은 무엇인가요?'
-                />
-                <button data-testid="new-todo-add-button"
-                        type='submit'
-                        disabled={!todoItem.trim()}>
-                ADD</button>
+                <TodoInputBox>
+                    <input data-testid="new-todo-input"
+                        onChange={handleTodoChange}
+                        value={todoItem}
+                        placeholder='오늘의 할 일을 적어주세요'
+                    />
+                    <button data-testid="new-todo-add-button"
+                            type='submit'
+                            disabled={!todoItem.trim()}>
+                    add</button>
+                </TodoInputBox>
             </form>
-        </>
+        </TodoInputWrapper>
     );
 }
 
+const TodoInputWrapper = styled.div`
+    form {
+        display: flex;
+    }
+`
+const TodoInputBox = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    width: 40rem;
+    height: 5.5rem;
+    background-color: white;
+    border-radius: 1.5rem;
+    box-shadow: #d7d7d7 2px 3px 5px;
+    padding: 0 1.5rem;
+
+    transition: width 0.3s, height 0.3s;
+
+    &:focus-within {
+        width: 43rem;
+        height: 7rem;
+        padding: 0 1.7rem;
+    }
+
+    input {
+        background-color: white;
+    }
+
+    button{
+        padding: 0.7rem 1.3rem;
+        margin: 0 0 0 5rem;
+        border-radius: 1rem;
+        background-color: orange;
+        color: white;
+        font-weight: 700;
+        text-transform: uppercase;
+        letter-spacing: 0.2rem;
+        transition: background-color 0.3s, color 0.3s;
+    }
+    button:hover{
+        background-color: #e77401;
+        color: white;
+        font-weight: 700;
+        text-transform: uppercase;
+        letter-spacing: 0.2rem;
+    }
+`
 export default TodoInput
--- client/src/@pages/todoPage.jsx
+++ client/src/@pages/todoPage.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import TodoList from '../@components/todo/todoList';
 import TodoInput from '../@components/todo/todoInput';
+import { styled } from 'styled-components';
+import TodoLogo from '../assets/todo.png';
 
 function TodoPage() {
     const [todos, setTodos] = useState([]);
@@ -11,11 +13,21 @@ function TodoPage() {
     };
 
     return(
-        <>
+        <TodoPageWrapper>
+            <img src={TodoLogo} alt='Signup Logo' width={400} height={50} style={{margin: '10rem 0 5rem 0'}}/>
             <TodoInput onAddTodo={handleAddTodo}/>
             <TodoList todos={todos}/>
-        </>
+        </TodoPageWrapper>
     )
 }
 
+const TodoPageWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+    height: 100vh;
+    background-color: #f7f7f7;
+
+`
 export default TodoPage
